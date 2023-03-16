bccb9670527301ef753c1a1288a4bd1ac32a8ebc
Amey Narkhede <ameynar@amazon.de>
2023-03-16 14:48:06 +0100

    Install libffi in CI

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, Extension, find_packages
-import os
 
 setup(
+    setup_requires=["cffi"],
+    packages=find_packages(),
+    ext_package="pyxorfilter",
     cffi_modules=["pyxorfilter/ffibuild.py:ffi"],
     test_suite="nose.collector",
     tests_require=["nose"],

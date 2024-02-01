7b7b08f9ec1b13cfb2bcc1bbfd993fc974c2bb33
Pedram Amini <pedram.amini@gmail.com>
2024-02-01 10:50:28 -0600

    Add serialization/deserialization test to test_fuse16.py
    Pulls large data from the Tranco list

--- tests/test_fuse16.py
+++ tests/test_fuse16.py
@@ -1,7 +1,48 @@
 from pyxorfilter import Fuse16
 from random import sample
 
+def test_fuse16_serialization():
+    import urllib.request
+    import tempfile
+    import os
 
+    # tranco list contains the top 1M most popular domain names.
+    url = "https://tranco-list.eu/download/LJ494/1000000"
+    
+    with urllib.request.urlopen(url) as response:
+        data = response.read().decode("utf-8", errors="replace")
+
+    # extract the domain list from the second column of the CSV (rank, domain)
+    domains = [line.split(",")[1] for line in data.splitlines()]
+
+    # populate XORF.
+    xor_filter = Fuse16(len(domains))
+    xor_filter.populate(domains)
+
+    # initial assertions.
+    needle = "google.com"
+    assert needle in domains
+    assert xor_filter.contains(needle)
+
+    # serialize to a temporary file.
+    path_serialized = tempfile.NamedTemporaryFile(delete=False).name
+
+    with open(path_serialized, "wb+") as fh:
+        fh.write(xor_filter.serialize())
+
+    # refresh XORF from disk.
+    with open(path_serialized, "rb") as fh:
+        deserialized_xor = Fuse16.deserialize(fh.read())
+
+    # is needle in the XORF?
+    try:
+        assert deserialized_xor.contains(needle)
+    except Exception as e:
+        raise e
+    finally:
+        # disk cleanup.
+        os.remove(path_serialized)
+    
 def test_fuse16_int():
     xor_filter = Fuse16(50)
     xor_filter.populate([_ for _ in range(50)])
